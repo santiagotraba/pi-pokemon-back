e653d8bb8f170409770c8bb253503fa00aea5174
Santiago <135069315+santiagotraba@users.noreply.github.com>
2024-06-15 22:49:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-require("dotenv").config()    
-const {PORT} = process.env 
+
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-    server.listen(PORT, "0.0.0.0", () => {
-        console.log("%s listening at 3001, prueba probando"); // eslint-disable-line no-console
+    server.listen(3001, "0.0.0.0", () => {
+        console.log("%s listening at 3001"); // eslint-disable-line no-console
     });
 });
